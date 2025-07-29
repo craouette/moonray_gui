07b26fbe41dfdba99227a06c3175ac5ee3fa80d0
206519669_nbcuni <jon.lanz@dreamworks.com>
2025-07-29 10:34:59 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '17.18'
+    _version = '17.19'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -43,7 +43,7 @@ conf_CI_variants = variants
 
 requires = [
     'mkl',
-    'moonray-17.18',
+    'moonray-17.19',
     'mcrt_denoise-6.10',
     'opencolorio-2',
     'qt',
